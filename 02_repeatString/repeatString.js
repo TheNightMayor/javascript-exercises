742f7fb9ca110af4979f0c748430dc391a5692d3
Lawrence <lawrencekeeganleblanc@gmail.com>
2021-11-30 14:51:08 -0600

    21/11/30-14:50-reinitialized git

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(input, num) {
+    string = input;
+    if (num === 0) {
+        return ''
+    } else if (num < 0) {
+        return 'ERROR'
+    } 
+    for (let i=1; i < num; ++i){
+        string += input    
+    } 
+    return string; 
 };
-
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
 const reverseString = function(str) {
-
+    const arr = [];
+    for (let i=0; i<= str.length; ++i) {
+        let letter = str.slice (i,(i+1));
+        arr.push(letter);
+    } //put slice from string length position into array 
+    arr.reverse(); //reverse array
+    let reverseStr = arr.join();//put elements from array into string //return string 
+    reverseStr = reverseStr.replace(/,/g,''); //remove commas
+    return reverseStr;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
