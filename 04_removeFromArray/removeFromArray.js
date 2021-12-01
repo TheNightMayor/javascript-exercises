450e9fe5b000c5c75210a697bf5d5a7647685d20
Lawrence <lawrencekeeganleblanc@gmail.com>
2021-11-30 20:00:49 -0600

    2021/11/30-19:59 something clicked :)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function(initArray,...args) {
+        for(let i=0; i<=args.length; ++i) {
+            for(let j=0; j<=initArray.length; ++j) {
+                if (args[i] === initArray[j]) {
+                    initArray.splice(j,1);
+                }
+            }
+        }
+        return initArray;
+   }
 // Do not edit below this line
 module.exports = removeFromArray;
