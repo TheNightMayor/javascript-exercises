55354c600f4d0a7b52ee26971712cd6256e8eba9
Lawrence <lawrencekeeganleblanc@gmail.com>
2021-12-01 16:02:37 -0600

    2021/12/01-16:02-sumAll 4/6 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(...ints) {  //make integers into array
+    ints.sort(); 
+    const endInt = ints[1];   
+    for (let i = 0; i<endInt-2; ++i) {
+        if (ints[i] < 0) {
+            return 'ERROR';
+        } 
+        //else if () {
+        //    return 'ERROR';
+        //}
+         else {ints.splice(i+1,0,ints[i]+1)
+        }
+    }
+        //make loop to add numbers between integers to array
+    let sum = ints.reduce(findSum);
+        function findSum(total, value) {
+           return total + value;
+        }//add all numbers in array
+    
+    return sum //return sum
 };
-
 // Do not edit below this line
 module.exports = sumAll;
