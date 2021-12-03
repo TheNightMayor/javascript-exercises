22be22eef78e8ccf2f7ac8b6cb88396dd36a3e4d
Lawrence <lawrencekeeganleblanc@gmail.com>
2021-12-02 19:04:40 -0600

    21/12/02-19:03-done fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(num) {
+   if (num < 0) {
+       return 'OOPS';
+   }
+    let fib = [1,1];
+    for (let i=0; i<=num; ++i) {
+        fib[i+2]=(fib[i]+(fib[i+1]))
+    }
+  return fib[num-1];
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
