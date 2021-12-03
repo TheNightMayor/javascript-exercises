31145c2d2e51fab956f14dca042eb2bc4857e8c9
Lawrence <lawrencekeeganleblanc@gmail.com>
2021-12-02 18:33:35 -0600

    21/12/02-18:33-done palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+    let editedString = inputString.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    let lowerString = editedString.toLowerCase();
+    let finalString = lowerString.replace(/\s/g,'');
+    let wordArray = finalString.split('');
+    wordArray = wordArray.reverse();
+    let reversedWord = wordArray.join('');
+    if (reversedWord == finalString) {
+        return true;
+    } else return false;
 };
 
 // Do not edit below this line
